3f84537dbf33c8ec1ee3bdad8301f4c4af125dbb
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2019-11-19 21:12:25 +0200

    Add files via upload

---
+++ WsMsTest.cs
@@ -0,0 +1,24 @@
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29009.5
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "WsMsTest", "MsTest.csproj", "{88154FE9-DAE0-4547-8552-93F4738724BB}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{88154FE9-DAE0-4547-8552-93F4738724BB}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{88154FE9-DAE0-4547-8552-93F4738724BB}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{88154FE9-DAE0-4547-8552-93F4738724BB}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{88154FE9-DAE0-4547-8552-93F4738724BB}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {F4EAED1B-52B0-49DE-963B-9B0D1474FB53}
+	EndGlobalSection
+EndGlobal
